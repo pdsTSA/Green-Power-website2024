6ed0ad96e517c72ad727a0cef722861937a60cfb
Charlie Boalch <charlieboalch@gmail.com>
2024-01-30 20:50:20 -0500

    Add scrolling facts below splash screen
    Signed-off-by: charlieboalch <charlieboalch@gmail.com>

--- HomePage/home.js
+++ HomePage/home.js
@@ -1,18 +1,19 @@
 $(async () => {
-    await sleep(1000);
 
     let controller = new ScrollMagic.Controller({
         globalSceneOptions: {
-            triggerHook: 'onLeave'
+            triggerHook: 'onLeave',
         }
     });
+
+    initAnimations(controller);
     await logoAnimation(controller);
 })
 
 const logoAnimation = async (controller) => {
     let tween;
 
-    tween = TweenMax.to('#main-bar-background', 1, {
+    tween = TweenMax.to('#main-bar-background', 0.8, {
         opacity: 1
     });
 
@@ -24,9 +25,15 @@ const logoAnimation = async (controller) => {
 
     await sleep(1000);
 
-    tween = TweenMax.to('#main-bar-text', 1, {
-        x: toPX('50vw')
-    });
+    if ($(window).width() <= 700) {
+        tween = TweenMax.to('#main-bar-text', 0.5, {
+            opacity: 1
+        });
+    } else {
+        tween = TweenMax.to('#main-bar-text', 0.5, {
+            x: toPX('100vw')
+        });
+    }
 
     new ScrollMagic.Scene({
         duration: 0
@@ -34,6 +41,32 @@ const logoAnimation = async (controller) => {
         .setTween(tween)
         .addTo(controller)
 }
+
+const initAnimations = (controller) => {
+    // content panel 1
+    let wipeAnimation = new TimelineMax()
+        .to("#content-1-fact-1", 0.5, {x: toPX('100vw')})
+        // animate to second panel
+        .to("#content-panel-1-container", 1,   {x: "-33.333%", delay: 2})
+        .to("#content-1-fact-2", 0, {display: "flex"})
+        .to("#content-1-fact-2", 0.5, {x: toPX('100vw')})
+        // animate to third panel
+        .to("#content-panel-1-container", 1,   {x: "-66.666%", delay: 2})
+        .to("#content-1-fact-3", 0, {display: "flex"})
+        .to("#content-1-fact-3", 0.5, {x: toPX('100vw')})
+
+    new ScrollMagic.Scene({
+        triggerElement: "#content-panel-1",
+        triggerHook: "onLeave",
+        duration: "300%"
+    })
+        .setPin("#content-panel-1")
+        .setTween(wipeAnimation)
+        .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+}
+
+
 
 const sleep = (milliseconds) => {
     return new Promise(resolve => setTimeout(resolve, milliseconds))
